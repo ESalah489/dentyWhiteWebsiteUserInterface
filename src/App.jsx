11f6bd935d69014d48387de19f218211b94c17a5
ebrahim salah <ebrahimsalah489@gmail.com>
2025-07-01 15:08:18 +0300

    setup project for clinic website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div className="min-h-screen flex items-center justify-center bg-blue-600 text-white text-2xl font-bold">
+      Hello Tailwind + Vite + React!
+    </div>
+  )
+}
+
+export default App
